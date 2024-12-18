3b84729e3553bc351ab63d1e2fed7f7c641b8065
Gaston Danielsen <gastondanielsen85@gmail.com>
2024-12-18 16:35:18 -0300

    refactor: add module auth

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,15 +5,17 @@ import { UsersModule } from '../users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 import { envValidationSchema } from 'src/config/env-validation.config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
-    UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
       validationSchema: envValidationSchema,
     }),
+    AuthModule,
+    UsersModule,
     PrismaModule,
   ],
   controllers: [AppController],
